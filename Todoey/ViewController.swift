a7e4aa97cd70f2be965e8ca043091935667d69cf
developeroliver <developpeur.olive@gmail.com>
2024-04-14 15:45:36 +0200

    remove storyboard, add table view, customize tab bar and add checkmark to table view

--- Todoey/ViewController.swift
+++ Todoey/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    let reuseID = "CELL_ID"
+    let itemArray = ["Find Mike", "Buy eggs", "Destory Demogorgon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        layout()
+        setupTableView()
+        title = "Todoey"
     }
-
-
+    
 }
 
+// MARK: - Helpers
+extension ViewController {
+    
+    private func layout() {
+        view.backgroundColor = .systemBackground
+        title = "Todoey"
+    }
+    
+    private func setupTableView() {
+        let tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseID)
+        tableView.rowHeight = 60
+        self.tableView = tableView
+    }
+}
+
+// MARK: - DataSource and Delegate
+extension ViewController {
+     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseID, for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.accessoryType = .none
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
